84965011842ab206d875612b7ffbd8c340e4cdb6
김지후 <capycat0513@gmail.com>
2026-03-16 10:43:29 +0900

    Create ex06.js

---
+++ 20260311/ex06.js
@@ -0,0 +1,15 @@
+// 모든 프로그래밍 영어는 대소문자 구분
+var Var // 변수명으로 예약된건 사용 불가
+var a = 20;
+console.log(typeof(a));
+var b = "배준후_닷컴";
+console.log(typeof(b));
+var c = true;
+console.log(typeof(c));
+var d;
+console.log(typeof(d));
+var e = null;
+console.log(typeof(e));
+var f = {a:10,b:20};
+console.log(f.a)
+console.log(f.b)

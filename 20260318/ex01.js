5eba6e586ad4cf46210de8e99a0af68ce6d739f5
김지후 <capycat0513@gmail.com>
2026-03-18 12:28:38 +0900

    Create ex01.js

---
+++ 20260318/ex01.js
@@ -0,0 +1,20 @@
+var score=[];
+score.push(10);
+score.push(20);
+console.log(score);
+
+
+
+
+//값 변경 가능은 let 불가능이면 const,var이 저 2개로 분해됨 (이제 var 안씀)
+var a = 10;
+var a = 20; //두번 선언이 가능하다
+console.log(a)
+
+let b = 10;
+b = 20;
+console.log(b);
+
+const c = 30;
+c = 40;
+console.log(c);

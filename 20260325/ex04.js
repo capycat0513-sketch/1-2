69bfe963383cd97b89f54fdf1de696eb3c880288
김지후 <capycat0513@gmail.com>
2026-04-01 12:18:43 +0900

    Create ex04.js

---
+++ 20260325/ex04.js
@@ -0,0 +1,19 @@
+const num = 10;
+const test = 20;
+const testi = 30;
+const testis = 40;
+
+switch (testis) {
+    case 10:
+        console.log('10')
+        break;
+    case 20:
+        console.log('20')
+        break;
+    case 30:
+        console.log('30')
+        break;
+    default:
+        console.log('default')
+        break;
+}
